5b607d46f5508b336397ebd5cc670fb7499a59f6
Luciero <pedrohs@ufba.br>
2025-10-02 10:43:14 -0300

    atualizacao

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+lista_usuarios = ['Pedro','João','Maria','Mauro']
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -11,6 +13,10 @@ def home():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html',lista_usuarios=lista_usuarios)
 
 
 if __name__ == '__main__':
