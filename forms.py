c0e0b2eecc0bd0cb97ec46ea964cf3e7f9473f76
Luciero <pedrohs@ufba.br>
2025-10-07 13:24:23 -0300

    Adicionado forms login e criar conta

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    usuario = StringField('Usuário', validators=[DataRequired()], )
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha =  PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
+    confirmacao_senha = PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')\
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
+    botao_submit_login = SubmitField('Fazer Login')
+
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 lista_usuarios = ['Pedro','João','Maria','Mauro']
 
+
+app.config['SECRET_KEY'] = 'adca60707f946de523ded83a1cf470a5'
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -14,9 +17,19 @@ def home():
 def contato():
     return render_template('contato.html')
 
+
 @app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html',lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/criar_conta')
+def criar_conta():
+    return render_template('criar_conta.html')
 
 
 if __name__ == '__main__':
